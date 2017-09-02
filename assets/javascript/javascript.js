85d3524e1fbc742f34873f3abb3a9aace41c9a2f
matthewnguyen <matthewnguyen19@gmail.com>
2017-09-01 19:16:14 -0700

    javascript done, minimal css

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,75 +1,75 @@
-var game = {
+// var game = {
 
-	numCorrect: 0,
-	numWrong: 0,
-	currentQuestion: 0,
-	playerChoice: 0,
-	correctChoice: 0,
-	gameTimer: 0,
-	questionTimer: 0,
-	timePerQuestion: 3,
-	timeLeft: this.timePerQuestion,
-	nextQuestionTimer: 0,
-	clockRunning: false,
-	question1: ["Sample 1", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 1],
-	question2: ["Sample 2", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 4],
-	question3: ["Sample 3", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 3],
-	question4: ["Sample 4", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 1],
-	question5: ["Sample 5", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 4],
-	question6: ["Sample 6", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 2],
-	question7: ["Sample 7", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 3],
-	question: [],
+// 	numCorrect: 0,
+// 	numWrong: 0,
+// 	currentQuestion: 0,
+// 	playerChoice: 0,
+// 	correctChoice: 0,
+// 	gameTimer: 0,
+// 	questionTimer: 0,
+// 	timePerQuestion: 3,
+// 	timeLeft: this.timePerQuestion,
+// 	nextQuestionTimer: 0,
+// 	clockRunning: false,
+// 	question1: ["Sample 1", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 1],
+// 	question2: ["Sample 2", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 4],
+// 	question3: ["Sample 3", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 3],
+// 	question4: ["Sample 4", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 1],
+// 	question5: ["Sample 5", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 4],
+// 	question6: ["Sample 6", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 2],
+// 	question7: ["Sample 7", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 3],
+// 	question: [],
 
 
-	newGame: function() {
+// 	newGame: function() {
 
-	},
+// 	},
 
-	start: function() {
-		question = [this.question1, this.question2, this.question3, this.question4, this.question5, this.question6, this.question7];
-      	this.gameTimer = setInterval(this.nextQuestion, this.timePerQuestion*1000);
-  	},
+// 	start: function() {
+// 		question = [this.question1, this.question2, this.question3, this.question4, this.question5, this.question6, this.question7];
+//       	this.gameTimer = setInterval(this.nextQuestion, this.timePerQuestion*1000);
+//   	},
 
-  	nextQuestion: function() {
-  		$("#question").html(this.question[game.currentQuestion][0]);
-  		$("#answer1").html(this.question[game.currentQuestion][1]);
-  		$("#answer2").html(this.question[game.currentQuestion][2]);
-  		$("#answer3").html(this.question[game.currentQuestion][3]);
-  		$("#answer4").html(this.question[game.currentQuestion][4]);
-  		this.correctChoice = this.question[game.currentQuestion][5];
-  		$("#answer1").on("click", function(){
-  			game.checkAnswer(1);
-  		});
-  		$("#answer2").on("click", function(){
-  			game.checkAnswer(2);
-  		});
-  		$("#answer3").on("click", function(){
-  			game.checkAnswer(3);
-  		});
-  		$("#answer4").on("click", function(){
-  			game.checkAnswer(4);
-  		});
-  		game.timeLeft = this.timePerQuestion;
-  		game.questionTimer = setInterval(this.secondsLeft, 1000);
-  		game.currentQuestion++;
-  	},
+//   	nextQuestion: function() {
+//   		$("#question").html(this.question[game.currentQuestion][0]);
+//   		$("#answer1").html(this.question[game.currentQuestion][1]);
+//   		$("#answer2").html(this.question[game.currentQuestion][2]);
+//   		$("#answer3").html(this.question[game.currentQuestion][3]);
+//   		$("#answer4").html(this.question[game.currentQuestion][4]);
+//   		this.correctChoice = this.question[game.currentQuestion][5];
+//   		$("#answer1").on("click", function(){
+//   			game.checkAnswer(1);
+//   		});
+//   		$("#answer2").on("click", function(){
+//   			game.checkAnswer(2);
+//   		});
+//   		$("#answer3").on("click", function(){
+//   			game.checkAnswer(3);
+//   		});
+//   		$("#answer4").on("click", function(){
+//   			game.checkAnswer(4);
+//   		});
+//   		game.timeLeft = this.timePerQuestion;
+//   		game.questionTimer = setInterval(this.secondsLeft, 1000);
+//   		game.currentQuestion++;
+//   	},
 
-  	checkAnswer: function(choice) {
-  		if(choice == correctChoice) {
-  			game.numCorrect++;
-  		} else {
-  			game.numWrong++;
-  		}
-  		clearInterval(this.gameTimer);
-  		this.nextQuestionTimer = setTimeout(this.start, 3000);
-  	},
+//   	checkAnswer: function(choice) {
+//   		if(choice == correctChoice) {
+//   			game.numCorrect++;
+//   		} else {
+//   			game.numWrong++;
+//   		}
+//   		clearInterval(this.gameTimer);
+//   		this.nextQuestionTimer = setTimeout(this.start, 3000);
+//   	},
 
-  	secondsLeft: function() {
-  		this.timeLeft--;
-  		$("#timer").text(this.timeLeft);
+//   	secondsLeft: function() {
+//   		this.timeLeft--;
+//   		$("#timer").text(this.timeLeft);
 
-  	}
+//   	}
 
-}
+// }
 
-game.start();
+// game.start();
--- assets/javascript/javascript2.js
+++ assets/javascript/javascript2.js
@@ -5,66 +5,144 @@ var playerChoice = 0;
 var correntChoice = 0;
 var gameTimer = 0;
 var questionTimer = 0;
-var timePerQuestion = 5;
+var timePerQuestion = 30; //in seconds
 var timeLeft = timePerQuestion;
 var nextQuestionTimer = 0;
 var clockRunning = false;
-var question1 = ["Sample 1", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 1];
-var	question2 = ["Sample 2", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 4];
-var	question3 = ["Sample 3", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 3];
-var	question4 = ["Sample 4", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 1];
-var	question5 = ["Sample 5", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 4];
-var	question6 = ["Sample 6", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 2];
-var	question7 = ["Sample 7", "Sample 1", "Sample 1", "Sample 1", "Sample 1", 3];
+//Questions exist as arrays with data in this order:
+//[0] - question
+//[1] - [4] answer choices
+//[5] index of correct answer choice
+var question1 = ["What is the capital of Delaware?", "Dover", "Delaware City", "Harrington", "Milford", 1];
+var	question2 = ["What is the capital of Nebraska?", "North Platte", "Kearney", "Omaha", "Lincoln", 4];
+var	question3 = ["What is the capital of New York?", "Buffalo", "New York", "Albany", "Syracuse", 3];
+var	question4 = ["What is the capital of Utah?", "Salt Lake City", "Beaver", "Provo", "Brigham City", 1];
+var	question5 = ["What is the capital of Maine?", "Auburn", "Bangor", "Alfred", "Augusta", 4];
+var	question6 = ["What is the capital of Kentucky?", "Louisville", "Frankfort", "Lexington", "Richmond", 2];
+var	question7 = ["What is the capital of Pennsylvania?", "Allentown", "Philadelphia", "Harrisburg", "Reading", 3];
 var question = [];
 
 function newGame() {
 
 }
+
 function start() {
+	clearInterval(gameTimer);
 	question = [question1, question2, question3, question4, question5, question6, question7];
+	nextQuestion();
   	gameTimer = setInterval(nextQuestion, timePerQuestion*1000);
 }
 
 function nextQuestion(){
 		clearInterval(questionTimer);
-  		$("#question").html(question[currentQuestion][0]);
-  		$("#answer1").html(question[currentQuestion][1]);
-  		$("#answer2").html(question[currentQuestion][2]);
-  		$("#answer3").html(question[currentQuestion][3]);
-  		$("#answer4").html(question[currentQuestion][4]);
+		unborderAll();
+		$("#timer").removeClass("red");
+  		$("#question").html("<STRONG>" + question[currentQuestion][0] + "</STRONG>");
+  		$("#answer1")
+	  		.html(question[currentQuestion][1])
+	  		.on("click", function(){
+	  			checkAnswer(1);
+	  		})
+	  		.hover(function(){
+	  			$("#answer1").addClass("hover");
+	  		}, function() {
+	  			$("#answer1").removeClass("hover");
+	  		});
+  		$("#answer2")
+	  		.html(question[currentQuestion][2])
+	  		.on("click", function(){
+	  			checkAnswer(2);
+	  		})
+	  		.hover(function(){
+	  			$("#answer2").addClass("hover");
+	  		}, function() {
+	  			$("#answer2").removeClass("hover");
+	  		});
+  		$("#answer3")
+	  		.html(question[currentQuestion][3])
+	  		.on("click", function(){
+	  			checkAnswer(3);
+	  		})
+	  		.hover(function(){
+	  			$("#answer3").addClass("hover");
+	  		}, function() {
+	  			$("#answer3").removeClass("hover");
+	  		});
+  		$("#answer4")
+	  		.html(question[currentQuestion][4])
+	  		.on("click", function(){
+	  			checkAnswer(4);
+	  		})
+	  		.hover(function(){
+	  			$("#answer4").addClass("hover");
+	  		}, function() {
+	  			$("#answer4").removeClass("hover");
+	  		});
+	  	$("#result").empty();
   		correctChoice = question[currentQuestion][5];
-  		$("#answer1").on("click", function(){
-  			checkAnswer(1);
-  		});
-  		$("#answer2").on("click", function(){
-  			checkAnswer(2);
-  		});
-  		$("#answer3").on("click", function(){
-  			checkAnswer(3);
-  		});
-  		$("#answer4").on("click", function(){
-  			checkAnswer(4);
-  		});
-  		timeLeft = timePerQuestion;
+  		timeLeft = timePerQuestion - 1;
   		questionTimer = setInterval(secondsLeft, 1000);
+  		$("#timer").text(timeLeft);
   		currentQuestion++;
   	}
 
+function unbindAll() {
+	$("#answer1").unbind();
+	$("#answer2").unbind();
+	$("#answer3").unbind();
+	$("#answer4").unbind();
+}
+
+function unborderAll() {
+	$("#answer1").removeClass("hover");
+	$("#answer2").removeClass("hover");
+	$("#answer3").removeClass("hover");
+	$("#answer4").removeClass("hover");
+}
+
 function checkAnswer(choice) {
+	clearInterval(questionTimer);
+	unbindAll();
   		if(choice == correctChoice) {
   			numCorrect++;
+  			$("#result").html("Correct!");
   		} else {
   			numWrong++;
+  			$("#result").html("Incorrect!");
   		}
   		clearInterval(gameTimer);
-  		nextQuestionTimer = setTimeout(start, 3000);
+  		if(currentQuestion < question.length) {
+  			nextQuestionTimer = setTimeout(start, 2000);
+  		}
+  		else{
+  			nextQuestionTimer = setTimeout(endGame, 2000);
+  		}
   	}
 
 function secondsLeft() {
   		timeLeft--;
   		$("#timer").text(timeLeft);
+  		if(timeLeft < (timePerQuestion/5)) {
+  			$("#timer").addClass("red");
+  		}
+  		if(timeLeft == 0) {
+  			checkAnswer(0);
+  		}
   	}
 
-
-start();
+function endGame() {
+	$("#result").empty();
+	$("#correct").html("Correct: " + numCorrect);
+  	$("#incorrect").html("Incorrect: " + numWrong);
+  	$("#replay").html("Click here to replay trivia!")
+  		.on("click", function(){
+  			currentQuestion = 0;
+  			$("#replay").empty().unbind();
+  			$("#correct").empty();
+  			$("#incorrect").empty();
+  			numCorrect = 0;
+  			numWrong = 0;
+  			start();
+  		});
+}
+$(document).ready(start);
